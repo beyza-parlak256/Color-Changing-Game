486e28971f34b84f036b15b32dd80b703de8e535
beyza-parlak256 <beyza.parlak98@erzurum.edu.tr>
2022-10-10 19:31:33 +0300

    Random color to boxes

--- Color Changing Game/Assets/Scripts/SquareColors.cs
+++ Color Changing Game/Assets/Scripts/SquareColors.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class SquareColors : MonoBehaviour
 {
-    private MeshRenderer mesh;
+    MeshRenderer mesh;
     public Material Black, Blue, Green, Orange, Purple, Red, Yellow;
     void Start()
     {
         mesh = GetComponent<MeshRenderer>();
         GiveColor();
-        
+
+
     }
     void Update()
     {
@@ -19,9 +20,34 @@ public class SquareColors : MonoBehaviour
 
     void GiveColor()
     {
-        // 6 renk var, 7 ye kadar olaný alýr
-        int random = Random.Range(1, 7);
-        Debug.Log(random);
+        // 7 renk var, 8 ye kadar olaný alýr
+        int random = Random.Range(1, 8);
+
+        switch (random)
+        {
+            case 1:
+                mesh.material = Yellow;
+                break;
+            case 2:
+                mesh.material = Blue;
+                break;
+            case 3:
+                mesh.material = Purple;
+                break;
+            case 4:
+                mesh.material = Green;
+                break;
+            case 5:
+                mesh.material = Orange;
+                break;
+            case 6:
+                mesh.material = Red;
+                break;
+            case 7:
+                mesh.material = Black;
+                break;
+
+        }
 
     }
 }
